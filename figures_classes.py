c58b27da9fded18939d1348b4fdd6880a817438b
olegmavlyutov <krallakane@gmail.com>
2019-07-10 14:15:16 +0300

    added methods for all figures

--- figures_classes.py
+++ figures_classes.py
@@ -1,21 +1,28 @@
-# класс Figure с атрибутами color и side, принимающих параметры цвета и размера стороны (радиуса) геометрических фигур
+# импортируем число Пи и расчет корня из библиотеки math
+from math import pi, sqrt
+
+
+# класс Figure с атрибутами color и side принимает параметры цвета и размера стороны (радиуса) геометрических фигур
 class Figure:
     def __init__(self, color, side):
         self.color = color
         self.side = side
 
 
-# создаем три класса Square, Circle и Triangle, которые будут наследовать атрибуты родительского класса Figure
+# классы Square, Circle и Triangle наследуют атрибуты родительского класса Figure и считают площадь встроенными методами
 class Square(Figure):
-    pass
+    def square(self, side):
+        return side ** 2
 
 
 class Circle(Figure):
-    pass
+    def square(self, side):
+        return pi * side ** 2
 
 
 class Triangle(Figure):
-    pass
+    def square(self, side):
+        return sqrt(3) / 4 * side ** 2
 
 
 # создадим три фигуры из классов Square, Circle и Figure
@@ -27,3 +34,8 @@ third_figure = Triangle("yellow", 30)
 print("First figure is", first_figure.color, type(first_figure).__name__, "with side", first_figure.side)
 print("Second figure is", second_figure.color, type(second_figure).__name__, "with radius", second_figure.side)
 print("Third figure is", third_figure.color, type(third_figure).__name__, "with side", third_figure.side)
+
+# вывод площади фигур через методы, встроенные в классы каждой фигуры
+print(first_figure.square(side=first_figure.side))
+print(second_figure.square(side=second_figure.side))
+print(third_figure.square(side=third_figure.side))
