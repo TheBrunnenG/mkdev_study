488341ab1ed3d5f2f5cae87889f362ede3349e1b
olegmavlyutov <krallakane@gmail.com>
2019-07-10 12:59:22 +0300

    created three figures

--- figures_classes.py
+++ figures_classes.py
@@ -15,3 +15,11 @@ class Circle(Figure):
 class Triangle(Figure):
     pass
 
+
+first_figure = Square("red", 20)
+second_figure = Circle("orange", 15)
+third_figure = Triangle("yellow", 30)
+
+print("First figure is", first_figure.color, type(first_figure).__name__, "with side", first_figure.side)
+print("Second figure is", second_figure.color, type(second_figure).__name__, "with radius", second_figure.side)
+print("Third figure is", third_figure.color, type(third_figure).__name__, "with side", third_figure.side)
